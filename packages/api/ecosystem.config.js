3c1fbd61cc728b82d4f95dddf1a9d1ab15f923f4
Mr.DoSS <artfut10@gmail.com>
2022-08-13 22:59:29 +0300

    Fix

--- packages/api/ecosystem.config.js
+++ packages/api/ecosystem.config.js
@@ -3,7 +3,13 @@ module.exports = {
     {
       name: 'api',
       script: 'yarn start',
-      args: '',
+      env: {
+        MONGODB_URL: process.env.MONGODB_URL,
+        ETH_NETWORK: process.env.ETH_NETWORK,
+        INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
+        CONTRACT_OWNER_PK: process.env.CONTRACT_OWNER_PK,
+        CONTRACT_OWNER_ADDRESS: process.env.CONTRACT_OWNER_ADDRESS,
+      },
     },
   ],
 }
