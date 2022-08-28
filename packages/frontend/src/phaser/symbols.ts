d2221f225fc365d1bd356a9ea872cf7d5a597950
Mr.DoSS <artfut10@gmail.com>
2022-08-28 12:26:26 +0300

    Try

--- packages/frontend/src/phaser/symbols.ts
+++ packages/frontend/src/phaser/symbols.ts
@@ -2,61 +2,61 @@ export const SYMBOLS = [
   {
     id: 1,
     name: 'Oni',
-    image: '/symbols/oni.png',
+    image: '/symbols-1/oni.png',
   },
   {
     id: 2,
     name: 'Samurai',
-    image: '/symbols/samurai.png',
+    image: '/symbols-1/samurai.png',
   },
   {
     id: 3,
     name: 'Gold Coin',
-    image: '/symbols/gold-coin.png',
+    image: '/symbols-1/gold-coin.png',
   },
   {
     id: 4,
     name: 'Card',
-    image: '/symbols/card.png',
+    image: '/symbols-1/card.png',
   },
   {
     id: 5,
     name: 'Girl on the Bridge',
-    image: '/symbols/girl-bridge.png',
+    image: '/symbols-1/girl-bridge.png',
   },
   {
     id: 6,
     name: 'Banzai Tree',
-    image: '/symbols/banzai-tree.png',
+    image: '/symbols-1/banzai-tree.png',
   },
   {
     id: 7,
     name: 'Couble Dice',
-    image: '/symbols/couble-dice.png',
+    image: '/symbols-1/couble-dice.png',
   },
   {
     id: 8,
     name: 'Katana',
-    image: '/symbols/katana.png',
+    image: '/symbols-1/katana.png',
   },
   {
     id: 9,
     name: 'Clubs',
-    image: '/symbols/clubs.png',
+    image: '/symbols-1/clubs.png',
   },
   {
     id: 10,
     name: 'Helmet',
-    image: '/symbols/helmet.png',
+    image: '/symbols-1/helmet.png',
   },
   {
     id: 11,
     name: 'Heart',
-    image: '/symbols/heart.png',
+    image: '/symbols-1/heart.png',
   },
   {
     id: 12,
     name: 'Fish',
-    image: '/symbols/fish.png',
+    image: '/symbols-1/fish.png',
   },
 ]
